df66965b651f4be9bb25eb4a855202dc88e15999
Andrii Tkach <andtkach@gmail.com>
2024-06-10 15:58:44 +0200

    Update connection string

--- code/Mango.Services.EmailAPI/Migrations/20240610134734_Init.cs
+++ code/Mango.Services.EmailAPI/Migrations/20240610134734_Init.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Mango.Services.EmailAPI.Migrations
 {
     /// <inheritdoc />
-    public partial class AddEmailLogTable : Migration
+    public partial class Init : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
